b5bacf420aa4923b52772bf5ec7e7f5d103a326c
Kysohee18 <hanashahky@gmail.com>
2026-06-21 22:09:27 +0700

    add bug lagi

--- DAL.cs
+++ DAL.cs
@@ -27,7 +27,7 @@ namespace CrudMahasiswaADO
             }
             return localIP;
         }
-        private static string connectionString = @"Data Source=.\PUTRASQL; Initial Catalog=DBAkademikADO; Integrated Security=True; TrustServerCertificate=True";
+        private static string connectionString = $"Data Source={GetLocalIPAddress()}; Initial Catalog=DBAkademikADO; User ID=sa; Password=passwordsa";
         private SqlConnection conn = new SqlConnection(connectionString);
 
         private SqlDataAdapter da;
